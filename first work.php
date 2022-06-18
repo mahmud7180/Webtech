eb4a83068df6f812d373e409c23b196273848db2
Mahmud Hasan <107356365+mahmud7180@users.noreply.github.com>
2022-06-18 23:21:38 +0600

    Add files via upload

---
+++ first work.php
@@ -0,0 +1,6 @@
+<?php
+$v = $_GET["id"]-5;
+echo $v."<br>";
+//echo "My is:". $_GET["id"]."<br>";
+//echo "My cgpa:". $_GET["cgpa"]."<br>";
+?>
---
+++ link.php
@@ -0,0 +1,11 @@
+<?php
+for($i=1;$i<=10;$i++)
+{
+	$a=$i+5;
+	$isODD = true;
+	if
+	?>
+	<a href = 'firstwork.php?id=<?php echo $a;?>'>Click here!</a><br>
+	<?php
+}
+?>
