7b0b9ad61184324406e559abac435af62d1531e0
Mahmud Hasan <107356365+mahmud7180@users.noreply.github.com>
2022-06-18 23:15:51 +0600

    Add files via upload

---
+++ Footer.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <fieldset>
+        <center>
+    
+                <h2> Copyright &copy 2022 </h2>
+            
+</center>
+</fieldset>
+</body>
+</html>
---
+++ Header.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <fieldset>
+        <table>      
+            <tr>
+            <td> <img src='../Files/sitelogo.jpg' alt='Site Logo'> </td> 
+            <td>  <H1> Wed-site Event Management &nbsp; &nbsp; &nbsp; </H1> </td>
+            <td> <a href="../View/home.php">  Home </a>
+            <td>  <a href="../View/login.php"> Moderator </a>
+            <td> <a href="../View/adminlogin.php" > Admin </a>
+            <td> <a href="../View/clientlogin.php" > Client </a>
+            <td> <a href="../View/vendorlogin.php" > Vendor </a>
+            </tr>
+        </table>    
+    </fieldset>
+    </body>
+</html>
